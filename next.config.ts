730b14fa30009f4fc33f5c4255d7bb99252fa20a
Hoang Huy Duc <huyduc1602@gmail.com>
2025-03-04 19:02:09 +0700

    Set output to 'export' in Next.js configuration and clean up fallback asset paths

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,18 @@ import withPWA from 'next-pwa';
 
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
 };
 
 export default withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development', // Chỉ kích hoạt khi build production
   fallbacks: {
     document: '/_offline',
     image: '/static/images/fallback.png',
-    audio: '/static/audio/fallback.mp3', 
-    video: '/static/video/fallback.mp4', 
+    audio: '/static/audio/fallback.mp3',
+    video: '/static/video/fallback.mp4',
     font: '/static/fonts/fallback.woff2',
   },
   ...nextConfig,
